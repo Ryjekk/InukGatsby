27a9084e7a9f1b3bd93477a730e079641e7f11ef
Inuk Ryjewsky <ryjewski@gmail.com>
2020-12-12 21:15:34 +0100

    work data

---
+++ src/db/workplace.js
@@ -0,0 +1,48 @@
+export const workspace = [
+  {
+    header_main: "Working at CareerToGo",
+    paragraph_main_one:
+      "I had the opportunity to work at CareerToGo, a Startup in Denmark for my 2019 Internship where I gained a position of Frontend Developer afterwards. This was an amazing experience where I was able to work alongside senior Developers and experienced Designers to build amazing Job platform for youth",
+    paragraph_main_two:
+      "Throughout this time I was pushed and challenged in every aspect of my skills and walked away as a much more refined developer.",
+    img_me: "",
+    header_goal: "Goal of work",
+    paragraph_goal_one:
+      "Prior to arriving at CareerToGo, I set some personal goals for myself so that I had clear objectives on what I wanted to improve on and accomplish. Of these goals, my main priority involved improving my abilities with VueJS.",
+    paragraph_goal_two:
+      "Another specific goal I had for my time in Denmark was improving my abilities as a member of a team. I wanted to understand how every phase of the development process works, from ideation to launch. This included grasping more advanced concepts of git, such as rebasing, checkouts, and reverting.",
+    img_stack: "",
+    header_challenges: "Personal Challenges and Growth",
+    paragraph_challenges_one:
+      "Time at CareerToGo was one of the most challenging and rewarding experiences I've ever had. Traveling overseas and experiencing a completely new team taught me so much about myself and about the professional I want to be.",
+    paragraph_challenges_two:
+      "At CareerToGo, I learned a ton about VueJS, NuxtJS, TailwindCSS, MySQL, PHP, Git, you name it, but I also learned so much about how to be a developer with integrity and a valuable member of a team. Working alongside extremely talented developers inspired me and pushed my bounds of what I believed any developer can do.",
+    img_interiors: "",
+    header_contribution: "My Contributions",
+    paragraph_contribution_one:
+      "During the time at CareerToGo i was responsible one for building and transforming designs to code. I worked alongside a designer and app developer, to create a full experience for the user of app.",
+    header_overview: "Overview",
+    paragraph_overview:
+      "My experience at CareerToGo will be something I remember forever, and I want to thank everyone at CareerToGo for the time they took teaching me so many new lessons and giving me such an amazing experience.",
+  },
+  {
+    header_main: "Studying at Salt",
+    paragraph_main_one:
+      "I had the opportunity join amazing Salt school in Sweden, a premium school for developers. This was an amazing experience where I was able to study and explore new coding concepts alongside best qualified teachers",
+    paragraph_main_two:
+      "Throughout this time I was pushed and challenged in every aspect of my skills and walked away as a much more refined developer.",
+    img_me: "",
+    header_goal: "Goal of School",
+    paragraph_goal_one: "",
+    paragraph_goal_two: "",
+    img_stack: "",
+    header_challenges: "Personal Challenges and Growth",
+    paragraph_challenges_one: "",
+    paragraph_challenges_two: "",
+    img_interiors: "",
+    header_contribution: "My Contributions",
+    paragraph_contribution_one: "",
+    header_overview: "Overview",
+    paragraph_overview: "",
+  },
+];
