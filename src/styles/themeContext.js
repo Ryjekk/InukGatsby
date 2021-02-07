049790e8876204e16bc727dbab310eb035282ecd
Inuk Ryjewsky <ryjewski@gmail.com>
2021-02-07 13:48:43 +0100

    adding cards

--- src/styles/themeContext.js
+++ src/styles/themeContext.js
@@ -2,14 +2,14 @@ const lightMode = {
   main: '#EEEEEE',
   secondary: 'rgb(34, 81, 208)',
   inverse: '#121212',
-  alt: '#F4F4F4',
-  altWhite: '#FFFFFF',
-  textMain: '#000000',
+  // alt: '#F4F4F4',
+  // altWhite: '#FFFFFF',
+  // textMain: '#000000',
   textLight: '#606060',
-  shadow: '0px 0px 40.9182px rgba(0, 0, 0, 0.08)',
-  altShadow: '0px 0px 39.221px rgba(0, 0, 0, 0.08)',
-  togglerFill: '#FFFFFF',
-  gradient: 'linear-gradient(0deg, #f4f4f4 0%, #ffffff 30%)'
+  // shadow: '0px 0px 40.9182px rgba(0, 0, 0, 0.08)',
+  // altShadow: '0px 0px 39.221px rgba(0, 0, 0, 0.08)',
+  // togglerFill: '#FFFFFF',
+  gradient: 'linear-gradient(0deg, #f4f4f4 0%, #fafafa 30%)'
 };
 
 export const darkMode = {
@@ -17,14 +17,14 @@ export const darkMode = {
   secondary: '#DE8D14',
   inverse: '#EEEEEE',
   heading: '#EEEEEE',
-  alt: '#201E26',
-  altWhite: '#201E26',
-  textMain: '#FFFFFF',
+  // alt: '#201E26',
+  // altWhite: '#201E26',
+  // textMain: '#FFFFFF',
   textLight: '#A4A4A4',
-  gradient: '-webkit-linear-gradient(90deg, #1A181F 0%, #201E26 100%)',
-  shadow: 'none',
-  altShadow: 'none',
-  togglerFill: '#34313D'
+  gradient: '-webkit-linear-gradient(90deg, #1A181F 0%, #201E26 100%)'
+  // shadow: 'none',
+  // altShadow: 'none',
+  // togglerFill: '#34313D'
 };
 
 export default lightMode;
